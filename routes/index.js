01f8b60f9085d03d9cec7c2195e394150dceaaac
Shady Gabal <shady@nyu.edu>
2015-04-01 10:05:01 -0400

    added indicators

--- routes/index.js
+++ routes/index.js
@@ -17,30 +17,44 @@ router.get('/', function(req, res) {
 router.post('/message', function(req, res){
 
 	console.log(req);
-	client.messages.create({
-	    to: "+13472102276",
-	    from: "+18559561331",
-	    body: "Text received."
-	}, function(err, responseData) {
-		 if (!err) { // "err" is an error received during the request, if any
-	        // "responseData" is a JavaScript object containing data received from Twilio.
-	        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-	        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-	        console.log(responseData.from); // outputs "+14506667788"
-	        console.log(responseData.body); // outputs "word to your mother."
-	    }
-	    else{
-	    	console.log(err);
-	    }
-	});
+
+		var phoneNumber = "+13472102276";
+		var output = "";
+
+		client.messages.create({
+		    to: "+13472102276",
+		    from: "+18559561331",
+		    body: "Text received."
+		}, function(err, responseData) {
+			 if (!err) { // "err" is an error received during the request, if any
+		        // "responseData" is a JavaScript object containing data received from Twilio.
+		        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+		        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+		        console.log(responseData.from); // outputs "+14506667788"
+		        console.log(responseData.body); // outputs "word to your mother."
+				output += "Sent to " + phoneNumber + "\n";
+
+		    }
+		    else{
+		    	console.log(err);
+		    	output += "Error";
+		    }
+
+		    res.send(output);
+		});
+
+	output += "Sending to " + phoneNumber + "\n";
 
 });
 
 router.get('/message', function(req, res){
 	console.log(req);
 
+	var phoneNumber = "+13472102276";
+		var output = "";
+
 	client.messages.create({
-	    to: "+13472102276",
+	    to: phoneNumber,
 	    from: "+18559561331",
 	    body: "\nKnock knock. \n Who's there? \n God \n God who? \n Godzilla."
 	}, function(err, responseData) {
@@ -50,11 +64,19 @@ router.get('/message', function(req, res){
 	        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
 	        console.log(responseData.from); // outputs "+14506667788"
 	        console.log(responseData.body); // outputs "word to your mother."
+			output += "Sent to " + phoneNumber;
+
 	    }
 	    else{
 	    	console.log(err);
+	    	output += "Error";
+
 	    }
+	    res.send(output);
+
 	});
+
+	output += "Sending to " + phoneNumber + "\n";
 
 });
 
