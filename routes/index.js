896a0a3a2437c06f93623f970f013a5791fc7e7c
Shady Gabal <shady@nyu.edu>
2015-04-01 13:22:27 -0400

    adding receiving working

--- routes/index.js
+++ routes/index.js
@@ -81,9 +81,35 @@ router.get('/message', function(req, res){
 });
 
 router.get('/receiveMessage', function(req, res){
-	var resp = new twilio.TwimlResponse();
-	resp.sms("Received your text bro");
-	console.log(resp.toString());
+	// var resp = new twilio.TwimlResponse();
+	console.log(req.params);
+	var body = "Received your text bro";
+	var phoneNumber = "+13472102276";
+	client.messages.create({
+	    to: phoneNumber,
+	    from: "+18559561331",
+	    body: body
+	}, function(err, responseData) {
+		 if (!err) { // "err" is an error received during the request, if any
+	        // "responseData" is a JavaScript object containing data received from Twilio.
+	        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+	        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+	        console.log(responseData.from); // outputs "+14506667788"
+	        console.log(responseData.body); // outputs "word to your mother."
+			// output += "Sent to " + phoneNumber;
+
+	    }
+	    else{
+	    	console.log(err);
+	    	// output += "Error";
+
+	    }
+	    // res.send(output);
+
+	});
+
+
+	// console.log(resp.toString());
 });
 
 router.get('/app', function(req, res){ 
