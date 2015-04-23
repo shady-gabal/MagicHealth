c313a2b7f2f81f3e212b2e0a781f7741b1908d2e
Shady Gabal <shady@nyu.edu>
2015-04-23 13:49:37 -0400

    working

--- db/User.js
+++ db/User.js
@@ -5,9 +5,21 @@ var userSchema = new Schema({
 	phone_number: {type : String, required: true},
 	first_name: {type: String},
 	last_name : {type: String},
+
+	last_message_received : {type: String},
+	last_message_sent : {type: String},
+
 	has_subscribed: {type: Boolean, default: false},
 	subscribe_step : {type: Number, default: 0},
+
 	pregnant : {type: Boolean, default: false},
+	num_months_pregnant : {type: Number, default: 0},
+
+	has_child : {type: Boolean, default: false},
+	age_of_child: {type: Number, default: 0},
+
+	age: {type: Number, default: 0},
+
 	date_created: {type: Date, default: Date.now()},
 	last_time_messaged: {type: Date, default: Date.now()}
 });
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var twilio = require('twilio');
 var User = require('../db/User.js');
+var sprintf = require('sprintf-js').sprintf;
 
 var mongoose = require('../db/mongoose_connect.js');
 
@@ -15,8 +16,6 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-
-var questionsToSubscribe = ["What is your name?", "Hi %s. How old are you?", "Are you pregnant currently?", "Have you had a child?"];
 
 
 router.post('/message', function(req, res){
@@ -108,27 +107,28 @@ router.get('/receiveMessage', function(req, res){
 				console.log("Creating new account for phone number " + phoneNumber + "...");
 				//no current account, subscribe
 				var newUser = new User({
-					phone_number : phoneNumber
+					phone_number : phoneNumber,
+					last_message_received: messageReceived
 				});
 				newUser.save();
-				subscribe(newUser, res, resp);
+				receiveSubscribe(newUser, res, resp, messageReceived);
 			}
 			else{
+				user.last_message_received = messageReceived;
 				console.log("Retrieved account for phone number " + phoneNumber);
 
 				if (!user.has_subscribed){
-					subscribe(user, res, resp);
+					receiveSubscribe(user, res, resp, messageReceived);
 				}
 				else{
-					body = "How are you";
+					body = "Thank you for subscribing.";
 				}
 			}
 		}
 
 
 		if (body){
-			resp.message(body);
-			res.send(resp.toString());
+			sendMessageResp(resp, res, body, user);
 		}
 	});	
 
@@ -164,20 +164,132 @@ router.get('/receiveMessage', function(req, res){
 });
 
 
-
-function subscribe(user, res, resp){
-	console.log("Subscribing " + user.phone_number + "...");
+function sendSubscribe(user, res, resp, existingBody){
 	var index = user.subscribe_step;
-	var body;
+	var body = "";
 
-	if (index < questionsToSubscribe.length){
-		body = questionsToSubscribe[index];
+	if (existingBody)
+		body = existingBody + " ".
+
+	switch(index){
+		// case -1: //Welcome to MagicHealth. To subscribe, please text us 'subscribe'
+		// body = "Welcome to MagicHealth. To subscribe, please text us 'subscribe'."
+		// break;
+
+		case 0: //
+		body += "Welcome to MagicHealth. To subscribe, please text us 'subscribe'."
+		break;
+
+		case 1: //What is your name?
+		body += "What is your name?"
+		break;
+
+		case 2://Hi %s. How old are you?
+		body += "Hi " + user.first_name + ". How old are you?";
+		break;
+
+		case 3: //Are you pregnant currently?
+		body += "Are you pregnant currently?"
+		break;
+
+		case 4: //How long have you been pregnant?
+		body += "How long have you been pregnant, " + user.first_name + "?"
+		break;
+
+		case 6: //Do you have a child that is under 2 years old?
+		body += "Do you have a child that is under 2 years old?"
+		break;
+
+		case 7: //How old is your child?
+		body += "How old is your child?"
+		break;
+
+		case 10: //“What day of the week would you like to receive messages from us?”
+		body += "What day of the week would you like to receive messages from us?"
+		break;
+
+		case 11: //“At what time during the day would you like to receive messages from us?
+		body += "At what time during the day would you like to receive messages from us?"
+		break;
+
+		case 12:
+		body += "Thanks for subscribing " + user.first_name + "! We will start messaging you soon with helpful tips on ";
+		var pred = user.pregnant ? " having a healthy pregnancy." : "raising a healthy child.";
+		body += pred;
+		user.subscribe_step = 100;
+		user.save();
+		break;
+
+		default: //already subscribed. Resubscribe
+		body += "Our records show that you're already subscribed. If this is an error or you'd like to resubscribe, text us 'resubscribe'.";
+		break;
 	}
-	else{
-		body = "Sup baby";
-	}
-	resp.message(body);
-	res.send(resp.toString());
+
+	sendMessageResp(resp, res, body, user);
 }
 
+function receiveSubscribe(user, res, resp, messageReceived){
+	sprintf("Subscribing %s...", user.phone_number);
+
+	var index = user.subscribe_step;
+	var sendDidntUnderstand = true;
+	var didntUnderstand = "Sorry, we couldn't understand that.";
+
+	messageReceived = messageReceived.trim().toLowerCase();
+
+	switch(index){
+		case 0: //Welcome to MagicHealth. To subscribe, please text us 'subscribe'
+		if (messageReceived === 'subscribe'){
+			user.subscribe_step = 1;
+			user.save();
+		}
+		else{
+			if (!user.last_message_sent){//send didnt understand
+				sendDidntUnderstand = false;
+			}
+		}
+		break;
+
+		case 1: //What is your name?
+		break;
+
+		case 2://Hi %s. How old are you?
+		break;
+
+		case 3: //Are you pregnant currently?
+		break;
+
+		case 4: //How long have you been pregnant?
+		break;
+
+		case 6: //Do you have a child that is under 2 years old?
+		break;
+
+		case 7: //How old is your child?
+		break;
+
+		case 10: //“What day of the week would you like to receive messages from us?”
+		break;
+
+		case 11: //“At what time during the day would you like to receive messages from us?
+		break;
+
+		default: //already subscribed. Resubscribe
+		break;
+	}
+
+	if (sendDidntUnderstand)
+		sendSubscribe(user, res, resp, didntUnderstand);
+	else sendSubscribe(user, res, resp);
+
+	// sendMessageResp(resp, res, body, user);
+}
+
+function sendMessageResp(resp, res, body, user){
+	sprintf("Responding to message %s from %s with %s...", user.last_message, user.phone_number, body);
+	resp.message(body);
+	res.send(resp.toString());
+	user.last_message_sent = body;
+	user.last_time_messaged = Date.now();
+}
 module.exports = router;
