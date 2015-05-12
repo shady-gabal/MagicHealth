180ab143553c41fd72b0ed98acdc3fa8bd8a20cb
Shady Gabal <shady@nyu.edu>
2015-05-11 21:12:25 -0400

    final?

--- routes/index.js
+++ routes/index.js
@@ -307,13 +307,14 @@ function gaveBirth(user){
 
 function tookVaccine(user){
 	var item = user.sent_vaccine_updates[user.sent_vaccine_updates.length - 1];
+	console.log(user.sent_vaccine_updates);
+	console.log(item);
 	if (item && user.finished_vaccines.indexOf(item) == -1){
-		console.log(user.sent_vaccine_updates);
-		console.log(item);
+
 		user.finished_vaccines.push(item);
 		sendMessage(user.phone_number, "Got it. Good job!");
 	}
-	else sendMessage(user.phone_number, "Yep we already know.");
+	else sendMessage(user.phone_number, "Got it. Good job!");
 
 }
 
@@ -713,7 +714,6 @@ function sendCorrectTextUpdate(user){
 			else console.log("No ptu for week " + weekNum + " for user " + user.phone_number);
 		}
 	});
-
 }
 
 module.exports = router;
